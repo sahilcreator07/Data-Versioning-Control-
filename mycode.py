773e444d892a6ec06888b809f824a4043505e892
Sahil Gupta <2021.sahil.gupta@ves.ac.in>
2025-04-20 15:02:02 +0530

    added DVC and Version 1

--- mycode.py
+++ mycode.py
@@ -19,7 +19,7 @@ df.loc[len(df.index)] = new_row_loc2
 
 # Ensure the "data" directory exists at the root level
 data_dir = 'data'
-os.makedirs(data_dir, exist_ok=True)
+ 
 
 # Define the file path
 file_path = os.path.join(data_dir, 'sample_data.csv')
